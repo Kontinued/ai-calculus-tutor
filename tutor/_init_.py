e7b10d3b63e3c3aaf438e3fc73df38fc9fd93312
Kontinued <98498547+Kontinued@users.noreply.github.com>
2026-01-05 09:08:22 -0600

    Makes tutor/ a package

--- tutor/_init_.py
+++ tutor/_init_.py
@@ -1,1 +1,7 @@
+from tutor import sympy_solver, graph_plotter, teacher_persona
+from .state import TutorState
+from .solver import sympy_solver
+from .visualize import graph_plotter
+from .explain import teacher_persona
 
+__all__ = ["TutorState", "sympy_solver", "graph_plotter", "teacher_persona"]
