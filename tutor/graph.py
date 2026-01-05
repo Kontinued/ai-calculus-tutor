f833c6e8c89a5de551fdf603b7c27ba57d917633
Kontinued <98498547+Kontinued@users.noreply.github.com>
2026-01-05 09:06:53 -0600

    Creates graph for user interaction
    This defines the nodes that the StateGraph will have to function properly

--- tutor/graph.py
+++ tutor/graph.py
@@ -1,1 +1,18 @@
+from langgraph.graph import StateGraph, END
+from langgraph.checkpoint.memory import MemorySaver
+from tutor.state import TutorState
+from tutor.solver import sympy_solver
+from tutor.visualize import graph_plotter
+from tutor.explain import teacher_persona
 
+graph = StateGraph(TutorState)
+graph.add_node("solve", sympy_solver)
+graph.add_node("visualize", graph_plotter)
+graph.add_node("explain", teacher_explainer)
+
+graph.set_entry_point("solve")
+graph.add_edge("solve", "visualize")
+graph.add_edge("visualize", "explain")
+graph.add_edge("explain", END)
+
+app = graph.compile(checkpointer=MemorySaver())
